263a401df8dc7d796dabe0dea003779512de3abc
Rogopl <alf.ivan2002@gmail.com>
2021-06-12 04:35:00 -0700

    added laba07_tutorial

---
+++ _builds/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/rogopl/Rogopl/workspace/projects/lab07_tutorial
+# Build directory: /home/rogopl/Rogopl/workspace/projects/lab07_tutorial/_builds
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(check "/home/rogopl/Rogopl/workspace/projects/lab07_tutorial/_builds/check")
+set_tests_properties(check PROPERTIES  _BACKTRACE_TRIPLES "/home/rogopl/Rogopl/workspace/projects/lab07_tutorial/CMakeLists.txt;66;add_test;/home/rogopl/Rogopl/workspace/projects/lab07_tutorial/CMakeLists.txt;0;")
